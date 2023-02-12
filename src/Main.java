a10d6f1a79b1f338d3c85bb566cc582e5099eaa0
aau8996 <auad@mail.uc.edu>
2023-02-12 16:48:23 -0500

    Final Code

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        // Variables/Inputs from the user
+        double firstNumber = 9;
+        double secondNumber = 9;
+        // Initial prompt from the program
+        System.out.println("What is your first number?");
+        System.out.println("What is your second number?");
+        // If firstNumber > secondNumber then
+        if(firstNumber > secondNumber){
+            // Output
+            System.out.println(secondNumber + " is less than " + firstNumber);
+        }
+        // else if firstNumber = secondNumber then
+        else if(firstNumber == secondNumber){
+            // Output
+            System.out.println(firstNumber + " is equal to " + secondNumber);
+        }
+        // else
+        else{
+            // Output
+            System.out.println(firstNumber + " is less than " + secondNumber);
+        }
+    }
+}
